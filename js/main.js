7e4990f5b81a9527ef8c76555674ed1fea8a2d72
Anna Ikonomova <ani.strandjeva@gmail.com>
2018-07-03 23:26:38 +0200

    Ficxing the alt for images

--- js/main.js
+++ js/main.js
@@ -156,7 +156,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = 'Image of ' + restaurant.name + ' restaurant';
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h2');
